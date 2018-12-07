5f8cbf1a2eefe8a9215d095313a987be76531364
l00pen <lovisa.skeppe@gmail.com>
2018-12-07 20:26:32 +0100

    Add two more vegetables

--- src/App.js
+++ src/App.js
@@ -1,32 +1,114 @@
 import React, { Component } from 'react';
-import logo from './vegetables/kale.png';
+import kaleImage from './vegetables/kale.png';
+import garlicImage from './vegetables/garlic.png';
+import minthImage from './vegetables/minth.png';
+
 import Tone from 'tone';
 import './App.css';
 
 class App extends Component {
-  componentDidMount() {
-    // var synth = new Tone.Synth().toMaster();
-    // var loop = new Tone.Loop(function(time){
-    //   synth.triggerAttackRelease("C2", "8n", time);
-    // }, "4n");
-    // loop.start("1m").stop("4m");
-    // Tone.Transport.start();
+  constructor() {
+    super();
+    this.state = {
+      garlic: false,
+      kale: false,
+      minth: false,
+      synth: new Tone.Synth().toMaster(),
+      bass: new Tone.MembraneSynth({
+        oscillator : {
+          type : 'triangle8'
+        },
+        envelope : {
+          attack : 2,
+          decay : 1,
+          sustain: 0.4,
+          release: 4
+        }
+      }).toMaster(),
+      minthSynth: new Tone.PolySynth(6, Tone.Synth).toMaster(),
+      loop: new Tone.Loop(this.toneLoopCallback.bind(this), "4n"),
+    }
+
+    this.state.minthSynth.set("detune", -1200);
+
+    const distortion = new Tone.Distortion(0.4).toMaster();
+    const pwm = new Tone.PWMOscillator("Bb3").toMaster();
+
+    this.state.synth.connect(distortion);
+    // this.state.synth.connect(pwm);
+
+    this.state.loop.start();
+    Tone.Transport.start();
   }
-  
+
+  toneLoopCallback(time) {
+    if (this.state.garlic) {
+      console.log(time * 2, Tone.Time('8n'), time)
+      this.state.synth.triggerAttackRelease('C4', '4n', time * 2)
+      this.state.synth.triggerAttackRelease('E4', '8n', time + Tone.Time('8n'))
+      this.state.synth.triggerAttackRelease('E4', '8n', time + Tone.Time('8n'))
+      this.state.synth.triggerAttackRelease('G4', '16n', time)
+      this.state.synth.triggerAttackRelease('B4', '16n', time + Tone.Time('8t'))
+      this.state.synth.triggerAttackRelease('G4', '16', time + Tone.Time('8t') * 2)
+      this.state.synth.triggerAttackRelease('E4', '2n', time)
+    }
+
+    if (this.state.kale) {
+      this.state.bass.triggerAttackRelease("E2", "8n", time);
+    }
+
+    if (this.state.minth) {
+      var chord = new Tone.Event(function(time, chord){
+        //the chord as well as the exact time of the event
+        //are passed in as arguments to the callback function
+      }, ["D4", "E4", "F4"]);
+      //start the chord at the beginning of the transport timeline
+      chord.start(time);
+      //loop it every measure for 8 measures
+      chord.loop = 8;
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <div className="App-logo">
-            <img src={logo} className="Kale-Pop" alt="logo" />
-          </div>
           <p>
             Pick a vegetable
           </p>
-          <div>
-            <button>TOMATO</button>
-            <button>SELLERI</button>
-            <button>AVOCADO</button>
+          <div className="Controls">
+            <button
+              onClick={() => this.setState({ kale: !this.state.kale })}
+            >
+              KALE
+            </button>
+            <button
+              onClick={() => this.setState({ garlic: !this.state.garlic })}
+            >
+              GARLIC
+            </button>
+            <button
+              onClick={() => this.setState({ minth: !this.state.minth })}
+            >
+              MINTH
+            </button>
+          </div>
+          <div className="Vegetable-Band">
+            { this.state.kale &&
+              <div className="Kale-Wrapper App-logo">
+                <img src={kaleImage} className="Kale-Pop" alt="kale" />
+              </div>
+            }
+            { this.state.garlic &&
+              <div className="Garlic-Wrapper App-logo">
+                <img src={garlicImage} className="Garlic-Salsa" alt="garlic" />
+              </div>
+            }
+            { this.state.minth &&
+              <div className="Minth-Wrapper">
+                <img src={minthImage} className="Minth-Salsa" alt="minth" />
+              </div>
+            }
           </div>
         </header>
       </div>
