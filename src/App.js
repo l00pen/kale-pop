c24b9e9e33422a56ff1f2fe7e507353ecbf113c9
l00pen <lovisa.skeppe@gmail.com>
2018-12-07 17:48:53 +0100

    Add Tone.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import logo from './vegetables/kale.png';
-import music from './ma_lill_loop.mp3';
+import Tone from 'tone';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    // var synth = new Tone.Synth().toMaster();
+    // var loop = new Tone.Loop(function(time){
+    //   synth.triggerAttackRelease("C2", "8n", time);
+    // }, "4n");
+    // loop.start("1m").stop("4m");
+    // Tone.Transport.start();
+  }
+  
   render() {
     return (
       <div className="App">
@@ -20,11 +29,6 @@ class App extends Component {
             <button>AVOCADO</button>
           </div>
         </header>
-        <audio
-          // autoPlay
-          loop
-          src={music}
-        />
       </div>
     );
   }
