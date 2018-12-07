16565387a09d18e29c801d50efa247d4e160fe36
l00pen <lovisa.skeppe@gmail.com>
2018-12-07 21:42:05 +0100

    Updates

---
+++ precache-manifest.16bea66d072f901458a7fcad1107beb7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "42e84a28b0d0ab218c04c9551ddd07ba",
+    "url": "/kale-pop/static/media/mint.42e84a28.png"
+  },
+  {
+    "revision": "25994c4807b48cca33326c2ca5260fa2",
+    "url": "/kale-pop/static/media/kale.25994c48.png"
+  },
+  {
+    "revision": "13277a0385e5fc989df1ce2b06d3af8b",
+    "url": "/kale-pop/static/media/garlic.13277a03.png"
+  },
+  {
+    "revision": "b32341005e5c36a56fd1",
+    "url": "/kale-pop/static/js/runtime~main.b3234100.js"
+  },
+  {
+    "revision": "94124b9d91474f96f1d0",
+    "url": "/kale-pop/static/js/main.94124b9d.chunk.js"
+  },
+  {
+    "revision": "ad7197e0aab7794c561e",
+    "url": "/kale-pop/static/js/1.ad7197e0.chunk.js"
+  },
+  {
+    "revision": "94124b9d91474f96f1d0",
+    "url": "/kale-pop/static/css/main.2e69f960.chunk.css"
+  },
+  {
+    "revision": "8cbd9fc9fe4634b0443de07d2c04c6dd",
+    "url": "/kale-pop/index.html"
+  }
+];
