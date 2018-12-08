0866a783f9ff5777b5d97df886d8cb3d3f59cbfb
l00pen <lovisa.skeppe@gmail.com>
2018-12-08 11:50:16 +0100

    Generalize veggies instruments

--- src/App.js
+++ src/App.js
@@ -36,6 +36,30 @@ function setupKaleInstrument() {
   }).toMaster();
 }
 
+function garlicLoop(garlic, time) {
+  if (garlic.isPlaying) {
+    garlic.instrument.triggerAttackRelease('C4', '4n', time * 2)
+    garlic.instrument.triggerAttackRelease('E4', '8n', time + Tone.Time('8n'))
+    garlic.instrument.triggerAttackRelease('E4', '8n', time + Tone.Time('8n'))
+    garlic.instrument.triggerAttackRelease('G4', '16n', time)
+    garlic.instrument.triggerAttackRelease('B4', '16n', time + Tone.Time('8t'))
+    garlic.instrument.triggerAttackRelease('G4', '16', time + Tone.Time('8t') * 2)
+    garlic.instrument.triggerAttackRelease('E4', '2n', time)
+  }
+}
+
+function kaleLoop(kale, time) {
+  if (kale.isPlaying) {
+    kale.instrument.triggerAttackRelease("E2", "8n", time);
+  }
+}
+
+function mintLoop(mint, time) {
+  if (mint.isPlaying) {
+    mint.instrument.triggerAttackRelease("G4", "2n", time);
+  }
+}
+
 class App extends Component {
   constructor() {
     super();
@@ -46,50 +70,47 @@ class App extends Component {
           label: 'GARLIC',
           isPlaying: false,
           instrument: setupGarlicInstrument(),
+          onLoop: garlicLoop,
         },
         kale: {
           id: 'kale',
           label: 'KALE',
           isPlaying: false,
           instrument: setupKaleInstrument(),
+          onLoop: kaleLoop,
         },
         mint: {
           id: 'mint',
           label: 'MINT',
           isPlaying: false,
           instrument: setupMintInstrument(),
+          onLoop: mintLoop,
         },
       },
       loop: new Tone.Loop(this.toneLoopCallback.bind(this), "4n"),
-      mute: false,
+      isMuted: false,
       isPlaying: false,
     }
 
     this.state.loop.start();
     Tone.Transport.start();
+
+    this.toggleIsAnyVegetablePlaying = this.toggleIsAnyVegetablePlaying.bind(this);
   }
 
   toneLoopCallback(time) {
-    const { kale, garlic, mint } = this.state.vegetables;
-    if (garlic.isPlaying) {
-      garlic.instrument.triggerAttackRelease('C4', '4n', time * 2)
-      garlic.instrument.triggerAttackRelease('E4', '8n', time + Tone.Time('8n'))
-      garlic.instrument.triggerAttackRelease('E4', '8n', time + Tone.Time('8n'))
-      garlic.instrument.triggerAttackRelease('G4', '16n', time)
-      garlic.instrument.triggerAttackRelease('B4', '16n', time + Tone.Time('8t'))
-      garlic.instrument.triggerAttackRelease('G4', '16', time + Tone.Time('8t') * 2)
-      garlic.instrument.triggerAttackRelease('E4', '2n', time)
-    }
+    const veggieValueArray = Object.values(this.state.vegetables);
 
-    if (kale.isPlaying) {
-      kale.instrument.triggerAttackRelease("E2", "8n", time);
-    }
+    veggieValueArray.map((veggie) => veggie.onLoop(veggie, time));
+    this.toggleIsAnyVegetablePlaying(veggieValueArray)
+  }
 
-    if (mint.isPlaying) {
-      mint.instrument.triggerAttackRelease("G4", "2n", time);
-    }
+  toggleIsAnyVegetablePlaying(veggieValueArray) {
+    const isAnyVeggiePlaying = veggieValueArray.reduce((mem, veggie) => {
+      return mem || veggie.isPlaying;
+    }, false);
 
-    this.setState({ isPlaying: mint.isPlaying || kale.isPlaying || garlic.isPlaying });
+    this.setState({ isPlaying: isAnyVeggiePlaying });
   }
 
   toggleVegetablePlay(veggie) {
@@ -102,8 +123,8 @@ class App extends Component {
   }
 
   toggleMute() {
-    this.setState({ mute: !this.state.mute }, () => {
-      Tone.Master.mute = this.state.mute;
+    this.setState({ isMuted: !this.state.isMuted }, () => {
+      Tone.Master.mute = this.state.isMuted;
     })
   }
 
