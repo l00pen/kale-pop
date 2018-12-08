1735b86e77b0ac285ce18164fe8e31d15701dae9
l00pen <lovisa.skeppe@gmail.com>
2018-12-08 12:01:42 +0100

    Make specific vegetable knowledge in App unnecessary

--- src/App.js
+++ src/App.js
@@ -3,90 +3,16 @@ import Tone from 'tone';
 
 import PlayControls from './PlayControls'
 import Controls from './Controls'
-import Kale from './vegetables/Kale';
-import Garlic from './vegetables/Garlic';
-import Mint from './vegetables/Mint';
+
+import PlayableVegetables from './PlayableVegetables';
 
 import './App.css';
-
-function setupMintInstrument() {
-  const polySynth = new Tone.PolySynth(6, Tone.Synth).toMaster();
-  polySynth.set("detune", -1200);
-  return polySynth
-}
-
-function setupGarlicInstrument() {
-  const distortion = new Tone.Distortion(0.4).toMaster();
-  const synth = new Tone.Synth().toMaster();
-  synth.connect(distortion);
-  return synth;
-}
-
-function setupKaleInstrument() {
-  return new Tone.MembraneSynth({
-    oscillator : {
-      type : 'triangle8'
-    },
-    envelope : {
-      attack : 2,
-      decay : 1,
-      sustain: 0.4,
-      release: 4
-    }
-  }).toMaster();
-}
-
-function garlicLoop(garlic, time) {
-  if (garlic.isPlaying) {
-    garlic.instrument.triggerAttackRelease('C4', '4n', time * 2)
-    garlic.instrument.triggerAttackRelease('E4', '8n', time + Tone.Time('8n'))
-    garlic.instrument.triggerAttackRelease('E4', '8n', time + Tone.Time('8n'))
-    garlic.instrument.triggerAttackRelease('G4', '16n', time)
-    garlic.instrument.triggerAttackRelease('B4', '16n', time + Tone.Time('8t'))
-    garlic.instrument.triggerAttackRelease('G4', '16', time + Tone.Time('8t') * 2)
-    garlic.instrument.triggerAttackRelease('E4', '2n', time)
-  }
-}
-
-function kaleLoop(kale, time) {
-  if (kale.isPlaying) {
-    kale.instrument.triggerAttackRelease("E2", "8n", time);
-  }
-}
-
-function mintLoop(mint, time) {
-  if (mint.isPlaying) {
-    mint.instrument.triggerAttackRelease("G4", "2n", time);
-  }
-}
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      vegetables: {
-        garlic: {
-          id: 'garlic',
-          label: 'GARLIC',
-          isPlaying: false,
-          instrument: setupGarlicInstrument(),
-          onLoop: garlicLoop,
-        },
-        kale: {
-          id: 'kale',
-          label: 'KALE',
-          isPlaying: false,
-          instrument: setupKaleInstrument(),
-          onLoop: kaleLoop,
-        },
-        mint: {
-          id: 'mint',
-          label: 'MINT',
-          isPlaying: false,
-          instrument: setupMintInstrument(),
-          onLoop: mintLoop,
-        },
-      },
+      vegetables: PlayableVegetables,
       loop: new Tone.Loop(this.toneLoopCallback.bind(this), "4n"),
       isMuted: false,
       isPlaying: false,
@@ -129,7 +55,6 @@ class App extends Component {
   }
 
   render() {
-    const { kale, garlic, mint } = this.state.vegetables;
     const vegetableArray = Object.values(this.state.vegetables)
     return (
       <div className="App">
@@ -143,9 +68,9 @@ class App extends Component {
             toggleVegetable={this.toggleVegetablePlay.bind(this)}
           />
           <div className="Vegetable-Band">
-            { kale.isPlaying && <Kale />}
-            { garlic.isPlaying && <Garlic />}
-            { mint.isPlaying && <Mint />}
+            { vegetableArray.map(({ isPlaying, tag: Tag, id }, i) => (
+              isPlaying && <Tag key={id} />
+            ))}
           </div>
         </header>
       </div>
---
+++ src/PlayableVegetables.js
@@ -0,0 +1,85 @@
+import Tone from 'tone';
+
+import Kale from './vegetables/Kale';
+import Garlic from './vegetables/Garlic';
+import Mint from './vegetables/Mint';
+
+function setupMintInstrument() {
+  const polySynth = new Tone.PolySynth(6, Tone.Synth).toMaster();
+  polySynth.set("detune", -1200);
+  return polySynth
+}
+
+function setupGarlicInstrument() {
+  const distortion = new Tone.Distortion(0.4).toMaster();
+  const synth = new Tone.Synth().toMaster();
+  synth.connect(distortion);
+  return synth;
+}
+
+function setupKaleInstrument() {
+  return new Tone.MembraneSynth({
+    oscillator : {
+      type : 'triangle8'
+    },
+    envelope : {
+      attack : 2,
+      decay : 1,
+      sustain: 0.4,
+      release: 4
+    }
+  }).toMaster();
+}
+
+function garlicLoop(garlic, time) {
+  if (garlic.isPlaying) {
+    garlic.instrument.triggerAttackRelease('C4', '4n', time * 2)
+    garlic.instrument.triggerAttackRelease('E4', '8n', time + Tone.Time('8n'))
+    garlic.instrument.triggerAttackRelease('E4', '8n', time + Tone.Time('8n'))
+    garlic.instrument.triggerAttackRelease('G4', '16n', time)
+    garlic.instrument.triggerAttackRelease('B4', '16n', time + Tone.Time('8t'))
+    garlic.instrument.triggerAttackRelease('G4', '16', time + Tone.Time('8t') * 2)
+    garlic.instrument.triggerAttackRelease('E4', '2n', time)
+  }
+}
+
+function kaleLoop(kale, time) {
+  if (kale.isPlaying) {
+    kale.instrument.triggerAttackRelease("E2", "8n", time);
+  }
+}
+
+function mintLoop(mint, time) {
+  if (mint.isPlaying) {
+    mint.instrument.triggerAttackRelease("G4", "2n", time);
+  }
+}
+
+const vegetables = {
+  garlic: {
+    id: 'garlic',
+    label: 'GARLIC',
+    isPlaying: false,
+    instrument: setupGarlicInstrument(),
+    onLoop: garlicLoop,
+    tag: Garlic,
+  },
+  kale: {
+    id: 'kale',
+    label: 'KALE',
+    isPlaying: false,
+    instrument: setupKaleInstrument(),
+    onLoop: kaleLoop,
+    tag: Kale,
+  },
+  mint: {
+    id: 'mint',
+    label: 'MINT',
+    isPlaying: false,
+    instrument: setupMintInstrument(),
+    onLoop: mintLoop,
+    tag: Mint,
+  },
+};
+
+export default vegetables;
